6e5c6c1dbcb78b390a2a8addbd674952914e86c2
anamerasul <anamerasul@gmail.com>
2022-01-28 14:28:37 +0600

    recap

---
+++ variable-array-con.js
@@ -0,0 +1,38 @@
+var bottleColor ="red";
+
+var waterQuantity =1;
+
+var isFull =false;
+
+
+console.log(bottleColor);
+
+// array
+
+var items =['bottle','mug','paper','pen'];
+
+items.indexOf('paper');
+
+items.indexOf('logence');
+
+items.push('envelop');
+items.push('watch');
+items.pop();
+items.pop();
+items.pop();
+
+
+
+// conditional
+
+if(items.length >=4){
+    console.log("you have to many ")
+}
+
+else if(items.length =2){
+console.log("woe")
+}
+
+else{
+    console.log("clean desk")
+}
