86f61d04d8cb019f5845784618958a2735597f0f
anamerasul <anamerasul@gmail.com>
2022-01-28 15:38:12 +0600

    more

---
+++ more_while_loop.js
@@ -0,0 +1,35 @@
+var number = 1; //loop variable
+
+// while (number <=10){
+//     console.log(number);
+//     number++;
+// }
+// while (number <=20){
+//     console.log(number);
+//     number++;
+// }
+// while (number <=20){
+//     console.log(number);
+//     number +=2;
+
+// }
+
+// while (number <=20){
+//     console.log(number);
+//     number =2*number+2;
+    
+// }
+
+number =0;
+
+// even number
+
+while (number <=50){
+    console.log(number);
+    number += 2;
+    
+}
+
+
+
+
