2b325f1e8b88142f7b126f9fa8f78e12170eb6dd
anamerasul <anamerasul@gmail.com>
2022-01-29 01:54:10 +0600

    index up

--- while_loop.js
+++ while_loop.js
@@ -3,7 +3,7 @@ var roastGiven =0;
 while(roastGiven<7){
     // console.log(roastGiven);
     console.log("roast den please");
-     roastGiven++;
-     console.log(roastGiven);
+    roastGiven++;
+    console.log(roastGiven);
 
 }
